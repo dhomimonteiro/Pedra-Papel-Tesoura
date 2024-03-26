f87edc9bb85b47bc85eb2fdb1cceec17a2e446f8
Dhomi Monteiro <dhomimonteiro@gmail.com>
2024-03-26 17:30:17 -0300

    push in repository

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const playerCardSpace = document.getElementById("player-cards-space");
 const pcCards = document.getElementsByClassName("pc-cards");
-const timer = document.getElementById('timer');
+const timerText = document.getElementById('timer');
 let isCardSelected = false;
-let cards = [];
-cards.push('./imgs/tesoura.png');
-cards.push('./imgs/papel.png');
-cards.push('./imgs/carta-costas.png');
+let cards = ['./imgs/tesoura.png', './imgs/papel.png', './imgs/carta-costas.png'];
+let countDown;
+let playerCardValue;
+let pcCardValue;
 
-//randomize where the cards are on hand
+//randomize the cards order on hand
 function randomizeHand() {
     //Array to prevent if the number generated was already used
     let haveIt = [];
     for (let i = 0; haveIt.length != 3; i++) {
-        let random = Math.floor(Math.random()*3)
+        let random = Math.floor(Math.random() * 3)
         if (!haveIt.includes(random)) {
             haveIt.push(random);
             const img = document.createElement('img');
             img.src = cards[random];
-            img.className =  "player-cards hover"
+            if (cards[random].includes('papel')) {
+                img.setAttribute('value', 'papel');
+            }else if(cards[random].includes('pedra')){
+                img.setAttribute('value', 'pedra');
+            }else{
+                img.setAttribute('value', 'tesoura');
+            }
+            img.className = "player-cards hover"
             playerCardSpace.appendChild(img);
         }
     }
@@ -28,22 +35,83 @@ randomizeHand()
 const playerCards = document.getElementsByClassName("player-cards");
 
 // select the player-card clicked
-function selectedPlayerCard(){
-    if (isCardSelected == false) {        
-        console.log('cliquei')
+function selectedPlayerCard() {
+    if (isCardSelected == false) {
         this.style.top = "-30px"
         isCardSelected = true;
+        playerCardValue = this.getAttribute('value')
+        
         removeHoverEffect();
+        activateTimer();
+        useCard(this);
     }
 }
 
+//activates the countdown
+function activateTimer() {
+    let seconds = 10;
+    pcPicksCard();
+    countDown = setInterval(function () {
+        timerText.textContent = seconds + "s";
+        seconds--;
+        if (seconds < 0) {
+            clearInterval(countDown);
+            timerText.textContent = "";
+            compareCards();
+        }
+    }, 1000);
+}
+
+//puts the selected card on the center of the table and push all the rest lower
+function useCard(card) {
+    setTimeout(() => {
+        card.classList += " userChoice"
+    }, 1000);
+}
 //remove hover effect when selecting a card
-function removeHoverEffect(){
+function removeHoverEffect() {
     for (let i = 0; i < playerCards.length; i++) {
         playerCards[i].classList.remove('hover');
     }
 }
 
+//click event on all player's cards
 for (let i = 0; i < playerCards.length; i++) {
     playerCards[i].addEventListener("click", selectedPlayerCard)
+}
+
+function pcPicksCard() {
+    cards = ['tesoura','papel','pedra'];
+    let random = Math.floor(Math.random() * 3);
+    pcCardValue = cards[random];
+}
+
+function compareCards() {
+    console.log('pc: ', pcCardValue);
+    console.log('player: ', playerCardValue);
+    if (pcCardValue === playerCardValue) {
+        timerText.textContent = "Empate";
+
+    } else if (pcCardValue === 'tesoura' && playerCardValue === 'pedra'){
+        console.log('Player venceu!')
+        timerText.textContent = "Empate";
+    }else if (pcCardValue === 'tesoura' && playerCardValue === 'papel'){
+        console.log('PC venceu!')
+        timerText.textContent = "Empate";
+    }else if (pcCardValue === 'pedra' && playerCardValue === 'tesoura'){
+        console.log('PC venceu!')
+        timerText.textContent = "Empate";
+    }else if (pcCardValue === 'pedra' && playerCardValue === 'papel'){
+        console.log('Player venceu!')
+        timerText.textContent = "Empate";
+    }else if (pcCardValue === 'papel' && playerCardValue === 'tesoura'){
+        console.log('Player venceu!')
+        timerText.textContent = "Empate";
+    }else if (pcCardValue === 'papel' && playerCardValue === 'pedra'){
+        console.log('PC venceu!')
+        timerText.textContent = "Empate";
+    }else{
+        console.log('teste pra ver se nd faltou')
+        timerText.textContent = "Empate";
+    }
 }
